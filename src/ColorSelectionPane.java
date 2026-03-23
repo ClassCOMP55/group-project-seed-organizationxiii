fbefa47737e210ba4ad591ba9be1094a0b214adb
Aiden Gonzalez <a_gonzalez87@u.pacific.edu>
2026-03-23 10:10:17 -0700

    the color selection pane

---
+++ src/ColorSelectionPane.java
@@ -0,0 +1,14 @@
+
+public class ColorSelectionPane extends GraphicsPane {
+
+	public ColorSelectionPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+
+	public void showContent() {
+	}
+
+	public void hideContent() {
+	}
+	
+}
