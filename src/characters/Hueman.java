fa5968eebde04b1c2e8c4edaea1eb43851b4b43e
Aiden Gonzalez <a_gonzalez87@u.pacific.edu>
2026-03-23 10:17:27 -0700

    actual player class

---
+++ src/characters/Hueman.java
@@ -0,0 +1,16 @@
+package characters;
+
+public class Hueman extends player{
+
+	public Hueman(int hp, int atk, int spd, String nm) {
+		super(hp, atk, spd, nm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean useAbility(int abilityID, Character target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
