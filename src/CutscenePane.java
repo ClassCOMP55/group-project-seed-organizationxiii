04bf6709a7da71e5b851b7570ded7091815b9aa6
Aiden Gonzalez <a_gonzalez87@u.pacific.edu>
2026-03-25 10:47:12 -0700

    better position

--- src/CutscenePane.java
+++ src/CutscenePane.java
@@ -7,10 +7,21 @@ public class CutscenePane extends GraphicsPane {
 	}
 
 	public void showContent() {
-		GImage hyperion = new GImage("hyperioncutscene.png");
-		
-		mainScreen.add(hyperion);
-		hyperion.scale(0.6, 0.6);
+	    GImage hyperion = new GImage("hyperioncutscene.png");
+
+	    // 1. Scale FIRST
+	    hyperion.scale(0.6, 0.6);
+
+	    // 2. Set position BEFORE adding
+	    double x = (MainApplication.WINDOW_WIDTH - hyperion.getWidth()) / 2;
+	    double y = (MainApplication.WINDOW_HEIGHT * 0.35) - (hyperion.getHeight() / 2);
+
+	    hyperion.setLocation(x, y);
+
+	    // 3. Add LAST
+	    mainScreen.add(hyperion);
+
+	    contents.add(hyperion);
 	}
 
 	public void hideContent() {
