58c7fedc57fa58242182068c4fd2c697ab213ab1
BeleAlomerovic <b_alomerovic@u.pacific.edu>
2026-03-27 09:39:22 -0700

    Added a back button, so we don't have to exit the application

--- src/CutscenePane.java
+++ src/CutscenePane.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GRect;
+import acm.graphics.GObject;
 
 public class CutscenePane extends GraphicsPane {
 	
@@ -20,14 +21,12 @@ public class CutscenePane extends GraphicsPane {
 		    "Guardians. Loathe convinced him to join his color terrorism.",
 		    "He's nearby and will attack at any moment",
 		    "Brace yourself."
-		};
+	};
 
-		private int dialogueIndex = 0;
-		private GLabel dialogueText;
-		
-	
-		
-		
+	private int dialogueIndex = 0;
+	private GLabel dialogueText;
+	private GRect backButton;
+	private GLabel backLabel;
 
 	public CutscenePane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
@@ -36,63 +35,82 @@ public class CutscenePane extends GraphicsPane {
 	public void showContent() {
 	    GImage hyperion = new GImage("hyperioncutscene.png");
 
-	    // 1. Scale FIRST
 	    hyperion.scale(0.6, 0.6);
 
-	    // 2. Set position BEFORE adding
 	    double x = (MainApplication.WINDOW_WIDTH - hyperion.getWidth()) / 2;
 	    double y = (MainApplication.WINDOW_HEIGHT * 0.35) - (hyperion.getHeight() / 2);
 
 	    hyperion.setLocation(x, y);
 
-	    // 3. Add LAST
 	    mainScreen.add(hyperion);
-
 	    contents.add(hyperion);
 	    
-	    //dialogueBox
 	    GRect dialogueBox = new GRect(
 	    	    50, 
 	    	    MainApplication.WINDOW_HEIGHT - 180, 
 	    	    MainApplication.WINDOW_WIDTH - 100, 
 	    	    130
-	    	);
+	    );
 
-	    	dialogueBox.setFilled(true);
-	    	dialogueBox.setFillColor(Color.WHITE);
-	    	dialogueBox.setColor(Color.BLACK);
+	    dialogueBox.setFilled(true);
+	    dialogueBox.setFillColor(Color.WHITE);
+	    dialogueBox.setColor(Color.BLACK);
 
-	    	mainScreen.add(dialogueBox);
-	    	contents.add(dialogueBox);
+	    mainScreen.add(dialogueBox);
+	    contents.add(dialogueBox);
 	    	
-	    //name of the box
-	    	GLabel nameLabel = new GLabel("Hyperion");
+	    GLabel nameLabel = new GLabel("Hyperion");
 
-	    	nameLabel.setFont("Times New Roman-Bold-25");
-	    	nameLabel.setColor(Color.BLACK);
+	    nameLabel.setFont("Times New Roman-Bold-25");
+	    nameLabel.setColor(Color.BLACK);
 
-	    	nameLabel.setLocation(
+	    nameLabel.setLocation(
 	    	    dialogueBox.getX() + 10,
 	    	    dialogueBox.getY() - 5
-	    	);
+	    );
 
-	    	mainScreen.add(nameLabel);
-	    	contents.add(nameLabel);
+	    mainScreen.add(nameLabel);
+	    contents.add(nameLabel);
 	    	
-	    	dialogueText = new GLabel(dialogue[0]);
+	    dialogueText = new GLabel(dialogue[0]);
 
-	    	dialogueText.setFont("Times New Roman-PLAIN-25");
-	    	dialogueText.setColor(Color.BLACK);
+	    dialogueText.setFont("Times New Roman-PLAIN-25");
+	    dialogueText.setColor(Color.BLACK);
 
-	    	dialogueText.setLocation(
+	    dialogueText.setLocation(
 	    	    dialogueBox.getX() + 10,
 	    	    dialogueBox.getY() + 40
-	    	);
+	    );
 
-	    	mainScreen.add(dialogueText);
-	    	contents.add(dialogueText);
-	    	
-	    
+	    mainScreen.add(dialogueText);
+	    contents.add(dialogueText);
+
+	    // Back button
+	    backButton = new GRect(
+	    	    dialogueBox.getX() + 10,
+	    	    dialogueBox.getY() + dialogueBox.getHeight() - 35,
+	    	    80,
+	    	    25
+	    );
+
+	    backButton.setFilled(true);
+	    backButton.setFillColor(Color.LIGHT_GRAY);
+	    backButton.setColor(Color.BLACK);
+
+	    mainScreen.add(backButton);
+	    contents.add(backButton);
+
+	    backLabel = new GLabel("Back");
+	    backLabel.setFont("Times New Roman-Bold-16");
+	    backLabel.setColor(Color.BLACK);
+
+	    backLabel.setLocation(
+	    	    backButton.getX() + 22,
+	    	    backButton.getY() + 18
+	    );
+
+	    mainScreen.add(backLabel);
+	    contents.add(backLabel);
 	}
 	
 	private void advanceDialogue() {
@@ -105,18 +123,28 @@ public class CutscenePane extends GraphicsPane {
 	    }
 	}
 
+	@Override
 	public void hideContent() {
-		
+	    for (GObject item : contents) {
+	        mainScreen.remove(item);
+	    }
+	    contents.clear();
 	}
-	
-	
 	private boolean pressed = false;
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 	    if (pressed) return;
-
 	    pressed = true;
+
+	    double x = e.getX();
+	    double y = e.getY();
+
+	    if (backButton.contains(x, y) || backLabel.contains(x, y)) {
+	        mainScreen.switchToColorSelectionScreen();
+	        return;
+	    }
+
 	    advanceDialogue();
 	}
 
