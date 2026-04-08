103dae4caf0a677bb060bc9b8350207465f2740d
BeleAlomerovic <b_alomerovic@u.pacific.edu>
2026-04-08 09:21:26 -0700

    Fixed the background color matched the design with the first battle pane

--- src/SecondBattlePane.java
+++ src/SecondBattlePane.java
@@ -52,6 +52,9 @@ public class SecondBattlePane extends GraphicsPane {
     @Override
     public void showContent() {
 
+        // =========================
+        // 1. Create characters
+        // =========================
         h1 = mainScreen.getPlayer();
         m1 = new MintSentinel(700, 24, 12, "Mint");
 
@@ -62,18 +65,21 @@ public class SecondBattlePane extends GraphicsPane {
         playerTurn = true;
         battleOver = false;
 
+        // =========================
+        // 2. Create images
+        // =========================
         huemanImage = new GImage(getHuemanImage());
         mintImage = new GImage("mint.png");
 
         huemanImage.scale(0.6);
-        mintImage.scale(0.6);
+        mintImage.scale(0.7);
 
         double hx = MainApplication.WINDOW_WIDTH * 0.25 - huemanImage.getWidth() / 2 + 30;
         double hy = 200;
         huemanImage.setLocation(hx, hy);
 
         double mx = MainApplication.WINDOW_WIDTH * 0.75 - mintImage.getWidth() / 2;
-        double my = 40;
+        double my = 10;
         mintImage.setLocation(mx, my);
 
         mainScreen.add(huemanImage);
@@ -82,9 +88,13 @@ public class SecondBattlePane extends GraphicsPane {
         contents.add(huemanImage);
         contents.add(mintImage);
 
+        // =========================
+        // 3. Create health bars
+        // =========================
         double barWidth = 200;
         double barHeight = 12;
 
+        // Hueman bar
         double hxBar = MainApplication.WINDOW_WIDTH / 2.2 - barWidth / 2;
         double hyBar = MainApplication.WINDOW_HEIGHT - barHeight - 150;
 
@@ -102,6 +112,7 @@ public class SecondBattlePane extends GraphicsPane {
         contents.add(huemanHealthBack);
         contents.add(huemanHealthBar);
 
+        // ===== SUPER METER =====
         double meterWidth = 120;
         double meterHeight = 12;
         double spacing = 15;
@@ -118,7 +129,7 @@ public class SecondBattlePane extends GraphicsPane {
         superMeterBar.setFillColor(Color.ORANGE);
 
         superLabel = new GLabel("SP");
-        superLabel.setFont("Times New Roman-Bold-10");
+        superLabel.setFont("Arial-Bold-10");
         superLabel.setLocation(smx, smy - 2);
 
         mainScreen.add(superMeterBack);
@@ -129,6 +140,7 @@ public class SecondBattlePane extends GraphicsPane {
         contents.add(superMeterBar);
         contents.add(superLabel);
 
+        // Mint bar
         double mxBar = 30;
         double myBar = 30;
 
@@ -138,7 +150,7 @@ public class SecondBattlePane extends GraphicsPane {
 
         mintHealthBar = new GRect(mxBar, myBar, barWidth, barHeight);
         mintHealthBar.setFilled(true);
-        mintHealthBar.setFillColor(Color.CYAN);
+        mintHealthBar.setFillColor(Color.RED);
 
         mainScreen.add(mintHealthBack);
         mainScreen.add(mintHealthBar);
@@ -146,210 +158,238 @@ public class SecondBattlePane extends GraphicsPane {
         contents.add(mintHealthBack);
         contents.add(mintHealthBar);
 
+        // =========================
+        // 4. Create names
+        // =========================
         huemanName = new GLabel(h1.getName());
-        huemanName.setFont("Times New Roman-Bold-14");
-        huemanName.setLocation(huemanHealthBack.getX(), huemanHealthBack.getY() - 5);
+        huemanName.setFont("Arial-Bold-14");
+        huemanName.setLocation(
+            huemanHealthBack.getX(),
+            huemanHealthBack.getY() - 5
+        );
 
         mintName = new GLabel(m1.getName());
-        mintName.setFont("Times New Roman-Bold-14");
-        mintName.setLocation(mintHealthBack.getX(), mintHealthBack.getY() - 5);
+        mintName.setFont("Arial-Bold-14");
+        mintName.setLocation(
+            mintHealthBack.getX(),
+            mintHealthBack.getY() - 5
+        );
 
         mainScreen.add(huemanName);
         mainScreen.add(mintName);
 
         contents.add(huemanName);
         contents.add(mintName);
-        
+
+        // =========================
+        // 5. Action box
+        // =========================
         double boxWidth = 300;
         double boxHeight = 120;
-        
+
         double boxX = 240;
         double boxY = MainApplication.WINDOW_HEIGHT - boxHeight - 30;
-        
+
+        // Shadow behind box
         GRect actionBoxShadow = new GRect(boxX + 6, boxY + 6, boxWidth, boxHeight);
         actionBoxShadow.setFilled(true);
-        actionBox.setFillColor(new Color(120, 120, 120));
-        actionBox.setColor(new Color(120, 120, 120));
+        actionBoxShadow.setFillColor(new Color(120, 120, 120));
+        actionBoxShadow.setColor(new Color(120, 120, 120));
         mainScreen.add(actionBoxShadow);
         contents.add(actionBoxShadow);
-        
+
+        // Main white box
         actionBox = new GRect(boxX, boxY, boxWidth, boxHeight);
         actionBox.setFilled(true);
         actionBox.setFillColor(Color.WHITE);
         actionBox.setColor(Color.BLACK);
         mainScreen.add(actionBox);
         contents.add(actionBox);
-        
+
+        // Inner gold frame
         GRect innerFrame = new GRect(boxX + 6, boxY + 6, boxWidth - 12, boxHeight - 12);
         innerFrame.setFilled(false);
         innerFrame.setColor(new Color(212, 175, 55));
         mainScreen.add(innerFrame);
         contents.add(innerFrame);
-        
+
         fightOption = new GLabel("FIGHT");
-        fightOption.setFont("Times New Roman-Bold-18");
+        fightOption.setFont("Arial-Bold-18");
         fightOption.setLocation(boxX + 20, boxY + 40);
-        
+
         mainScreen.add(fightOption);
         contents.add(fightOption);
-        
-        continueButton = new GRect(MainApplication.WINDOW_WIDTH - 180, MainApplication.WINDOW_HEIGHT - 50, 160, 25);
-        
+
+        // =========================
+        // 6. Continue button
+        // =========================
+        continueButton = new GRect(
+            MainApplication.WINDOW_WIDTH - 180,
+            MainApplication.WINDOW_HEIGHT - 50,
+            160,
+            25
+        );
+
         continueButton.setFilled(true);
         continueButton.setFillColor(Color.LIGHT_GRAY);
         continueButton.setColor(Color.BLACK);
-        
+
         mainScreen.add(continueButton);
         contents.add(continueButton);
-        
+
         continueLabel = new GLabel("Continue");
-        continueLabel.setFont("Times New Roman-Bold-16");
-        continueLabel.setLocation(continueButton.getX() + 35, continueButton.getY() + 18);
-        
+        continueLabel.setFont("Arial-Bold-16");
+        continueLabel.setLocation(
+            continueButton.getX() + 35,
+            continueButton.getY() + 18
+        );
+
         mainScreen.add(continueLabel);
         contents.add(continueLabel);
     }
-    
+
     @Override
     public void mouseClicked(MouseEvent e) {
-    	if (battleOver) return;
-    	
-    	GObject obj = mainScreen.getElementAtLocation(e.getX(), e.getY());
-    	
-    	if (obj == continueButton || obj == continueLabel) {
-    		mainScreen.switchToVictoryScreen();
-    		return;
-    	}
-    	
-    	if (!showingFightMenu) {
-    		if (obj == fightOption) {
-    			openFightMenu();
-    		}
-    	} else {
-    		if (obj == fightOption && playerTurn) {
-    			performPlayerAttack();
-    			checkBattleEnd();
-    			
-    			if (!battleOver) {
-    				performEnemyTurn();
-    				checkBattleEnd();
-    			}
-    		} else if (obj == abilityOption) {
-    			if (h1.getSuperMeter() > 0 && playerTurn) {
-    				h1.useAbility(1, m1);
-    				updateHealthBars();
-    				playerTurn = false;
-    				
-    				checkBattleEnd();
-    				
-    				if (!battleOver) {
-    					performEnemyTurn();
-    					checkBattleEnd();
-    				}
-    			}
-    		}
-    	}
+
+        if (battleOver) return;
+        GObject obj = mainScreen.getElementAtLocation(e.getX(), e.getY());
+
+        if (obj == continueButton || obj == continueLabel) {
+            mainScreen.switchToVictoryScreen();
+            return;
+        }
+
+        if (!showingFightMenu) {
+            if (obj == fightOption) {
+                openFightMenu();
+            }
+        } else {
+            if (obj == fightOption && playerTurn) {
+
+                performPlayerAttack();
+                checkBattleEnd();
+
+                if (!battleOver) {
+                    performEnemyTurn();
+                    checkBattleEnd();
+                }
+
+            } else if (obj == abilityOption) {
+                if (h1.getSuperMeter() > 0 && playerTurn) {
+                    h1.useAbility(1, m1);
+                    updateHealthBars();
+                    playerTurn = false;
+
+                    checkBattleEnd();
+
+                    if (!battleOver) {
+                        performEnemyTurn();
+                        checkBattleEnd();
+                    }
+                }
+            }
+        }
     }
-    
+
     private void openFightMenu() {
-    	showingFightMenu = true;
-    	
-    	mainScreen.remove(fightOption);
-    	contents.remove(fightOption);
-    	
-    	fightOption = new GLabel("Basic Attack");
-    	fightOption.setLocation(actionBox.getX() + 20, actionBox.getY() + 40);
-    	
-    	abilityOption = new GLabel("Superpower");
-    	abilityOption.setLocation(actionBox.getX() + 20, actionBox.getY() + 70);
-    	
-    	mainScreen.add(fightOption);
-    	mainScreen.add(abilityOption);
-    	
-    	contents.add(fightOption);
-    	contents.add(abilityOption);
+        showingFightMenu = true;
+
+        mainScreen.remove(fightOption);
+        contents.remove(fightOption);
+
+        fightOption = new GLabel("Basic Attack");
+        fightOption.setLocation(actionBox.getX() + 20, actionBox.getY() + 40);
+
+        abilityOption = new GLabel("Superpower");
+        abilityOption.setLocation(actionBox.getX() + 20, actionBox.getY() + 70);
+
+        mainScreen.add(fightOption);
+        mainScreen.add(abilityOption);
+
+        contents.add(fightOption);
+        contents.add(abilityOption);
     }
-    
+
     private void updateSuperMeter() {
-    	double ratio = (double) h1.getSuperMeter() / 500.0;
-    	superMeterBar.setSize(120 * ratio, 12);
+        double ratio = (double) h1.getSuperMeter() / 500.0;
+        superMeterBar.setSize(120 * ratio, 12);
     }
-    
+
     private void performPlayerAttack() {
-    	if (!playerTurn || battleOver) return;
-    	
-    	h1.attackOpponent(m1);
-    	updateHealthBars();
-    	
-    	playerTurn = false;
+        if (!playerTurn || battleOver) return;
+
+        h1.attackOpponent(m1);
+        updateHealthBars();
+
+        playerTurn = false;
     }
-    
+
     private void performEnemyTurn() {
-    	if (battleOver) return;
-    	
-    	if (m1.isAlive()) {
-    		m1.takeTurn(h1);
-    		updateHealthBars();
-    	}
-    	
-    	playerTurn = true;
+        if (battleOver) return;
+
+        if (m1.isAlive()) {
+            m1.takeTurn(h1);
+            updateHealthBars();
+        }
+
+        playerTurn = true;
     }
-    
+
     private void checkBattleEnd() {
-    	if (!m1.isAlive()) {
-    		battleOver = true;
-    		System.out.println("YOU WIN");
-    		mainScreen.switchToVictoryScreen();
-    		return;
-    	}
-    	
-    	if (!h1.isAlive()) {
-    		battleOver = true;
-    		System.out.println("YOU LOSE");
-    	}
+        if (!m1.isAlive()) {
+            battleOver = true;
+            System.out.println("YOU WIN");
+            mainScreen.switchToVictoryScreen();
+            return;
+        }
+
+        if (!h1.isAlive()) {
+            battleOver = true;
+            System.out.println("YOU LOSE");
+        }
     }
-    
+
     private void updateHealthBars() {
-    	double huemanRatio = (double) h1.getHP() / huemanMaxHP;
-    	double mintRatio = (double) m1.getHP() / mintMaxHP;
-    	
-    	huemanHealthBar.setSize(200 * huemanRatio, 12);
-    	mintHealthBar.setSize(200 * mintRatio, 12);
-    	
-    	updateSuperMeter();
+        double huemanRatio = (double) h1.getHP() / huemanMaxHP;
+        double mintRatio = (double) m1.getHP() / mintMaxHP;
+
+        huemanHealthBar.setSize(200 * huemanRatio, 12);
+        mintHealthBar.setSize(200 * mintRatio, 12);
+
+        updateSuperMeter();
     }
-	
+
     @Override
     public void keyPressed(KeyEvent e) {
-    	if (huemanImage == null) return;
-    	
-    	int key = e.getKeyCode();
-    	
-    	if (key == KeyEvent.VK_LEFT) {
-    		huemanImage.move(-10, 0);
-    	} else if (key == KeyEvent.VK_RIGHT) {
-    		huemanImage.move(10, 0);
-    	} else if (key == KeyEvent.VK_UP) {
-    		huemanImage.move(0, -10);
-    	} else if (key == KeyEvent.VK_DOWN) {
-    		huemanImage.move(0, 10);
-    	}
+        if (huemanImage == null) return;
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            huemanImage.move(-10, 0);
+        } else if (key == KeyEvent.VK_RIGHT) {
+            huemanImage.move(10, 0);
+        } else if (key == KeyEvent.VK_UP) {
+            huemanImage.move(0, -10);
+        } else if (key == KeyEvent.VK_DOWN) {
+            huemanImage.move(0, 10);
+        }
     }
-    
+
     private String getHuemanImage() {
-    	String selectedColor = mainScreen.getSelectedColor();
-    	
-    	if (selectedColor == null) return "redback.png";
-    	if (selectedColor.equals("red")) return "redback.png";
-    	if (selectedColor.equals("green")) return "greenback.png";
-    	return "blueback.png";
+        String selectedColor = mainScreen.getSelectedColor();
+
+        if (selectedColor == null) return "redback.png";
+        if (selectedColor.equals("red")) return "redback.png";
+        if (selectedColor.equals("green")) return "greenback.png";
+        return "blueback.png";
     }
-    
-	@Override
-	public void hideContent() {
-		for (GObject obj : contents) {
-			mainScreen.remove(obj);
-		}
-		contents.clear();
-	}
+
+    @Override
+    public void hideContent() {
+        for (GObject obj : contents) {
+            mainScreen.remove(obj);
+        }
+        contents.clear();
+    }
 }
