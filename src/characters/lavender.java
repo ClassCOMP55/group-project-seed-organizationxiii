ff4be7b8c97876d1cb88b599fd93abe9f73bcd54
Misbah Khan <misbahkhaan7@gmail.com>
2026-04-15 10:40:18 -0700

    lavender java created

--- src/characters/lavender.java
+++ src/characters/lavender.java
@@ -1,9 +1,15 @@
 package characters;
 
 public class lavender extends boss {
+	
+	private boolean usedFakeOut = false;
+	private boolean enraged = false;
+	
 
 	public lavender(int hp, int atk, int spd, String nm) {
 		super(hp, atk, spd, nm);
+		usedFakeOut = false;
+		loadSprites();
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -21,18 +27,61 @@ public class lavender extends boss {
 		sprites.put("main", "lavender.png");
 	}
 	
+	public void pollenBurst(Hueman player) {
+		System.out.print("Lavender used Pollen Burst!");
+		player.takeDamage((int)(calculateDamage() * 0.8));
+	}
+	
+	public void lavenderStorm(Hueman player) {
+		System.out.println("Lavender summoned a storm!");
+		player.takeDamage(calculateDamage());
+	}
+	
+	@Override
+	public int calculateDamage() {
+		if (enraged) {
+			return (int) (attack * 1.4);
+		} else {
+			return attack;
+		}
+	}
+	
+	@Override
+	public void takeDamage(int dmg) {
+		currentHP -= dmg;
+		
+		if (currentHP < halfOfHealth && !enraged) {
+			enraged = true;
+			System.out.println(name + " became enraged!");
+		}
+		
+		if (currentHP < 0) {
+			currentHP = 0;
+		}
+		
+		System.out.println(name + " took " + dmg + " damage!");
+	}
 	
 	@Override
 	public void takeTurn(Hueman player) {
-		
-		if (Math.random() < 0.33) {
+		if (!usedFakeOut) {
+			usedFakeOut = true;
 			fakeOut(player);
 			return;
 		}
 		
-		System.out.println("Lavender summoned a storm!");
-		attackOpponent(player);
-		
+		if (enraged) {
+			if (Math.random() < 0.4) {
+				pollenBurst(player);
+			} else {
+				lavenderStorm(player);
+			}
+		} else {
+			if (Math.random() < 0.33) {
+				pollenBurst(player);
+			} else {
+				lavenderStorm(player);
+			}
+		}
 	}
-
 }
