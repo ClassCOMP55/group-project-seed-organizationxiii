1a184b04b2833ce55cc7d5431921fdb6636c945d
Aiden Gonzalez <a_gonzalez87@u.pacific.edu>
2026-04-15 09:34:07 -0700

    lavender class

---
+++ src/characters/lavender.java
@@ -0,0 +1,36 @@
+package characters;
+
+public class lavender extends boss {
+
+	public lavender(int hp, int atk, int spd, String nm) {
+		super(hp, atk, spd, nm);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean fakeOut(Hueman player) {
+	    System.out.println("Lavender used Fake Out!");
+
+	    player.takeDamage((int)(attack * 0.5));
+
+	    System.out.println("Hueman flinched!");
+	    return true; // ALWAYS flinches
+	}
+	
+	
+	
+	
+	
+	@Override
+	public void takeTurn(Hueman player) {
+		
+		if (Math.random() < 0.33) {
+			fakeOut(player);
+			return;
+		}
+		
+		System.out.println("Lavender summoned a storm!");
+		attackOpponent(player);
+		
+	}
+
+}
