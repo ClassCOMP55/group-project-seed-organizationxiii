cbde8884d9af0a0758d2bd619a0ce117564cd1a8
Misbah Khan <misbahkhaan7@gmail.com>
2026-04-20 10:28:53 -0700

    hueman stronger in eighth battle pane

--- src/EighthBattlePane.java
+++ src/EighthBattlePane.java
@@ -140,6 +140,7 @@ public class EighthBattlePane extends GraphicsPane {
         updateLayoutScale();
 
         h1 = mainScreen.getPlayer();
+        h1.heal();
         enemy = new Decima(1600, 42, 22, "Decima", Decima.Phase.FINAL);
         huemanMaxHP = h1.getHP();
         enemyMaxHP = enemy.getHP();
